8c556ae5f90534ffaa7e0acb18cc8078c104bbdb
vijaysoul <evijay77@gmail.com>
2020-03-31 20:07:26 -0700

    ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh -l
 
-echo "I am afraid that I could not reproduce your issue. Both comments on both issues or pull requests can trigger the workflow with on: issue_comment.  
+sudo apt-get install matrix
 
-For us to narrow down the issue, check whether this issue happened in workflow scope ,repo scope or the whole account. Please try next things:
-
- 
-
-Add a comment in issues or pull requests again, will current workflow be triggered?
-Create a new workflow file with on: issue_comment in the same repository.
-Try in other repos in the same github account.
- 
-
-Please kindly let us know your results."
+matrix
